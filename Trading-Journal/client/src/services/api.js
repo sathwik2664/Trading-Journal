7fff57c0d496353875319feb6feb4e3f73c08a63
sathwik2664 <ruyyadisathwikreddy@email.com>
2026-06-04 16:49:43 +0530

    fix api base url

--- Trading-Journal/client/src/services/api.js
+++ Trading-Journal/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sathwiktrades-backend.onrender.com',
+  baseURL: 'https://sathwiktrades-backend.onrender.com/api',
 });
 
 export default api;
